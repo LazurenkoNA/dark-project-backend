b033a29f63463ef95fd25bf4d86cb211fa0cec5e
Nikita Lazurenko <nikita.lazureko.d@gmail.com>
2021-11-30 01:57:37 +0200

    - add authorization (jwt) - add roles - add users

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+(async () => {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+
+  const config = new DocumentBuilder()
+    .setTitle('Dark Club Backend')
+    .setDescription('REST API')
+    .setVersion('1.0.0')
+    .addTag('Tag Name')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
+})();
---
+++ src/users/models/users.model.ts
@@ -0,0 +1,52 @@
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '../../roles/models/roles.model';
+import { UserRoles } from '../../roles/models/user-roles.model';
+
+interface UserCreationAttributes {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationAttributes> {
+  @ApiProperty({ example: '1', description: 'Unique id' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ApiProperty({ example: 'test@gmail.com', description: 'Email address' })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  email: string;
+
+  @ApiProperty({ example: 'qwerty123', description: 'Password' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+
+  @ApiProperty({ example: 'true', description: 'Banned or not' })
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: false,
+  })
+  isBanned: boolean;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
+}
